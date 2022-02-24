62f257c054440419b16098dcf20da3365b3632ba
UsernameIsNotAvailable00 <79504892+UsernameIsNotAvailable00@users.noreply.github.com>
2022-02-24 13:33:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from time import *
+
+title1 = "welcome to"
+title1 = title1.upper()
+for letter in title1.center(60):
+  print(letter, end=" ")
+print()
+print()
+sleep(2)
+print("""`7MMF'     A     `7MF' .g8""8q.              `7MM\"""Yb.          `7MM\"""Mq. `7MMF'            db                `7MM\"""Yb.\n  `MA     ,MA     ,V .dP'    `YM.              MM    `Yb.          MM   `MM.  MM             ;MM:                 MM    `Yb.\n   VM:   ,VVM:   ,V  dM'      `MM `7MMpMMMb.   MM     `Mb  .gP"Ya  MM   ,M9   MM            ,V^MM.   `7MMpMMMb.   MM     `Mb\n    MM.  M' MM.  M'  MM        MM   MM    MM   MM      MM ,M'   Yb MMmmdM9    MM           ,M  `MM     MM    MM   MM      MM\n    `MM A'  `MM A'   MM.      ,MP   MM    MM   MM     ,MP 8M\"""\""" MM  YM.    MM      ,    AbmmmqMA    MM    MM   MM     ,MP\n     :MM;    :MM;    `Mb.    ,dP'   MM    MM   MM    ,dP' YM.    , MM   `Mb.  MM     ,M   A'     VML   MM    MM   MM    ,dP'\n      VF      VF       `"bmmd"'   .JMML  JMML.JMMmmmdP'    `Mbmmd.JMML. .JMM.JMMmmmmMMM .AMA.   .AMMA.JMML  JMML.JMMmmmdP'""")
+
